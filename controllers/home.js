fe8909a86cfc00ac587b445f636f055a5d325bd2
Geordie King <geordie.king@gmail.com>
2022-09-26 00:30:35 +0100

    added index and first landing page+partials

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,8 @@
 module.exports = {
     getIndex: (req,res)=>{
         res.render('index.ejs')
-    }
+  },
+  getLandingPage: (req,res)=>{
+    res.render('landingPage.ejs')
+  }
 }
--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,7 @@ const homeController = require('../controllers/home')
 // const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', homeController.getIndex)
+router.get('/landingPage', homeController.getLandingPage)
 // router.get('/login', authController.getLogin)
 // router.post('/login', authController.postLogin)
 // router.get('/logout', authController.logout)
