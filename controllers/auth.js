635ab99370c2b51df86a7c36ec6373800f9ead6e
Geordie King <geordie.king@gmail.com>
2022-10-03 01:20:56 +0100

    updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,17 @@
+require('dotenv').config({path: './config/.env'})
+const { response } = require('express')
+const knex = require('../config/database')
+
+
+// const knexResp = () => {
+//   knex('tblTest')
+//   .select('Comment')
+//   .where('Name', 'StartConn')
+//   .then(returnedRecords => console.log(`DB4 Says:  ${returnedRecords[0].Comment}`))
+//   .finally(function () { knex.destroy() })
+// }
+
+
 module.exports = {
   getLogin: (req, res) => {
     res.render('login.ejs')
@@ -9,12 +23,25 @@ module.exports = {
     const data = await req.body.email
     console.log('Hi from postLogin')
     console.log(req.body)
-    
   },
   postSignUp: async (req,res) => {
     console.log('Hi from postSignUp')
     console.log(req.body)
-    
+
+    // knexResp()
+
+    // await knex('tblTest')
+    // .select('Comment')
+    // .where('Name', 'StartConn')
+    // .then(returnedRecords => console.log(`DB3 Says:  ${returnedRecords[0].Comment}`))
+    // .finally(function () { knex.destroy() })
+
+    // knex('tblTest')
+    //   .insert({
+    //     Name: 'Slaughterhouse Five',
+    //     Comment: 'Book'
+    //   })
+
     res.render('landingPage.ejs')
   }
 }
